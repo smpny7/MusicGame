5d5fb5961ca28f07a3b5b31f6411fd2c30144457
smpny7 <symphonymode7@icloud.com>
2020-05-16 20:35:56 +0900

    初期コミット

---
+++ Assets/TimingMaker/CSVWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class CSVWriter : MonoBehaviour {
+
+	public string fileName; // 保存するファイル名
+
+	// CSVに書き込む処理
+	public void WriteCSV (string txt) {
+		StreamWriter streamWriter;
+		FileInfo fileInfo;
+		fileInfo = new FileInfo (Application.dataPath + "/" + fileName + ".csv");
+		streamWriter = fileInfo.AppendText ();
+		streamWriter.WriteLine (txt);
+		streamWriter.Flush ();
+		streamWriter.Close ();
+	}
+}
