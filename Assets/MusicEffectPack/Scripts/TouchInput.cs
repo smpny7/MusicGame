1af7e8390be683de1469d15d4a9736cd228ee23b
smpny7 <symphonymode7@icloud.com>
2020-06-04 15:02:29 +0900

    [update] 16:9スマホ対応

--- Assets/MusicEffectPack/Scripts/TouchInput.cs
+++ Assets/MusicEffectPack/Scripts/TouchInput.cs
@@ -13,24 +13,29 @@ public class TouchInput : MonoBehaviour {
 
     void Start () {
         Vector3 tmp1 = GameObject.Find ("Lane1").transform.localPosition;
-        _rect1.x = Screen.width / 2 + tmp1.x - 125;
-        _rect1.y = Screen.height / 2 + 150;
+        _rect1.x = Screen.width / 2 + tmp1.x - 85;
+        _rect1.y = Screen.height / 2 + 100;
+        _rect1.width = 170;
 
         Vector3 tmp2 = GameObject.Find ("Lane2").transform.localPosition;
-        _rect2.x = Screen.width / 2 + tmp2.x - 125;
-        _rect2.y = Screen.height / 2 + 150;
+        _rect2.x = Screen.width / 2 + tmp2.x - 85;
+        _rect2.y = Screen.height / 2 + 100;
+        _rect2.width = 170;
 
         Vector3 tmp3 = GameObject.Find ("Lane3").transform.localPosition;
-        _rect3.x = Screen.width / 2 + tmp3.x - 125;
-        _rect3.y = Screen.height / 2 + 150;
+        _rect3.x = Screen.width / 2 + tmp3.x - 85;
+        _rect3.y = Screen.height / 2 + 100;
+        _rect3.width = 170;
 
         Vector3 tmp4 = GameObject.Find ("Lane4").transform.localPosition;
-        _rect4.x = Screen.width / 2 + tmp4.x - 125;
-        _rect4.y = Screen.height / 2 + 150;
+        _rect4.x = Screen.width / 2 + tmp4.x - 85;
+        _rect4.y = Screen.height / 2 + 100;
+        _rect4.width = 170;
 
         Vector3 tmp5 = GameObject.Find ("Lane5").transform.localPosition;
-        _rect5.x = Screen.width / 2 + tmp5.x - 125;
-        _rect5.y = Screen.height / 2 + 150;
+        _rect5.x = Screen.width / 2 + tmp5.x - 85;
+        _rect5.y = Screen.height / 2 + 100;
+        _rect5.width = 170;
     }
 
     void OnGUI () {
